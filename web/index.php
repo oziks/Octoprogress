d21aa6da33b7688321d9604876944d608315aaa6
Denis Roussel <denis.roussel@gmail.com>
2012-10-17 11:24:56 +0200

    utilisation des paramètres de prod dans index.php

--- web/index.php
+++ web/index.php
@@ -5,6 +5,6 @@ ini_set('display_errors', 0);
 require_once __DIR__.'/../vendor/autoload.php';
 
 $app = require __DIR__.'/../app/bootstrap.php';
-require __DIR__.'/../app/dev.php';
+require __DIR__.'/../app/prod.php';
 require __DIR__.'/../src/controllers.php';
 $app->run();
