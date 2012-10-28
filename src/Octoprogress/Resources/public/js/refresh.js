daea8541787061dd5e093b41dfc96eb0de522a8c
Brunot Morgan <brunot.morgan@gmail.com>
2012-10-28 21:24:36 +0100

    mise à jour du thème :p

--- src/Octoprogress/Resources/public/js/refresh.js
+++ src/Octoprogress/Resources/public/js/refresh.js
@@ -2,7 +2,20 @@ $(document).ready(function() {
     var refresh = function() {
         $.post(refreshUri, function(html) {
             $('#content').html(html);
+
+            heighter();
         });
+    }
+
+    var heighter = function() {
+        var maxHeight = 0;
+        $('.project').each(function() {
+            if ($(this).height() > maxHeight) {
+                maxHeight = $(this).height();
+            }
+        });
+
+        $('.project').height(maxHeight);
     }
 
     refresh();
